274c99915bfef71575525aa0384a5a910907f784
贾浩文 <J18942651096@163.com>
2023-05-20 15:27:51 +0800

    一些小的修改

--- springboot_demo/src/main/java/com/jiahaowen/springboot_demo/SpringbootDemoApplication.java
+++ springboot_demo/src/main/java/com/jiahaowen/springboot_demo/SpringbootDemoApplication.java
@@ -2,12 +2,23 @@ package com.jiahaowen.springboot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+//Springboot引导类
+//整个程序的入口
+//作用：加载Bean
 @SpringBootApplication
 public class SpringbootDemoApplication {
-
+    //自动创建出来的长这个样子：
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
 
+    /*//解析引导类功能
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringbootDemoApplication.class, args);
+        Object bean = ctx.getBean(SpringbootDemoApplication.class);
+        System.out.println("Bean info =======>"+bean);
+    }
+    */
 }
