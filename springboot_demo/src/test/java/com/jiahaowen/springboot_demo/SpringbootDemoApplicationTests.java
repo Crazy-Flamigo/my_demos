35a364da9f1db6c0773db8e1ad1a1046a950fe73
贾浩文 <J18942651096@163.com>
2023-05-20 17:09:45 +0800

    yaml配置基础，JUnit整合

--- springboot_demo/src/test/java/com/jiahaowen/springboot_demo/SpringbootDemoApplicationTests.java
+++ springboot_demo/src/test/java/com/jiahaowen/springboot_demo/SpringbootDemoApplicationTests.java
@@ -3,6 +3,9 @@ package com.jiahaowen.springboot_demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//测试类找不到引导类解决办法：
+//@SpringBootTest 改成 @SpringBootTest(classes = SpringbootDemoApplication.class)
+
 @SpringBootTest
 class SpringbootDemoApplicationTests {
 
